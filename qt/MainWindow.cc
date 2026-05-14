7d6ce17f9fe3d877bc03e06a1a3cba25243be82d
Dan <dn.grig2005@gmail.com>
2026-05-14 21:34:09 +0300

    fixed issue #7970

--- qt/MainWindow.cc
+++ qt/MainWindow.cc
@@ -1307,9 +1307,9 @@ void MainWindow::addTorrents(QStringList const& filenames)
         {
             show_options = b->isChecked();
         }
-        QString dirname = file_dialog->directory().absolutePath();
-        prefs_.set(Prefs::OPEN_DIALOG_FOLDER, dirname);
+        prefs_.set(Prefs::OPEN_DIALOG_FOLDER, file_dialog->directory().absolutePath());
     }
+
     for (QString const& filename : filenames)
     {
         addTorrent(AddData{ filename }, show_options);
